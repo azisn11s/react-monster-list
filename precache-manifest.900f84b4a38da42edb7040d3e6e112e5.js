2201c58c376d5c62dfeb46fa3a16a9c8b7133cf0
Azis Novian <azisnovian.kom48@gmail.com>
2020-01-05 07:26:49 +0700

    Updates

---
+++ precache-manifest.900f84b4a38da42edb7040d3e6e112e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d7263477fd6c37f5cc871c544deaf7",
+    "url": "/react-monster-list/index.html"
+  },
+  {
+    "revision": "fbdbad1199291d6de114",
+    "url": "/react-monster-list/static/css/main.a510d6f5.chunk.css"
+  },
+  {
+    "revision": "aaccca230dcab823f8a4",
+    "url": "/react-monster-list/static/js/2.2030c856.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster-list/static/js/2.2030c856.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbdbad1199291d6de114",
+    "url": "/react-monster-list/static/js/main.57f8bdc6.chunk.js"
+  },
+  {
+    "revision": "201deb8187dcf379a248",
+    "url": "/react-monster-list/static/js/runtime-main.4f618c06.js"
+  }
+]);
